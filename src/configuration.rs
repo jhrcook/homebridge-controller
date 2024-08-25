1ce38c33cb62213a2cffb822593a657f141a2e0d
Josh Cook <joshuacook0023@gmail.com>
2024-08-25 06:37:09 -0400

    feat: update configuration for new features

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,16 +1,35 @@
 use serde::{Deserialize, Serialize};
 
+const fn _true() -> bool {
+    true
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TurningMorningLightsOffConfig {
+    #[serde(default = "_true")]
+    pub active: bool,
     pub duration: u32,
     pub off_time: Option<String>,
     pub after_sunrise: Option<i64>,
+    pub last_call_after_scheduled_off: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ControlEveningLightsConfig {
+    #[serde(default = "_true")]
     pub active: bool,
+    pub minutes_before_sunset_start: i64,
+    pub minutes_after_sunset_peak: i64,
+    pub minutes_after_sunset_finish: i64,
+    pub start_brightness: u8,
+    pub max_brightness: u8,
+    pub final_brightness: u8,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Configuration {
     pub turn_morning_lights_off: TurningMorningLightsOffConfig,
+    pub control_evening_lights: ControlEveningLightsConfig,
     pub program_loop_pause: f32,
     pub ip_address: String,
     pub latitude: f32,
